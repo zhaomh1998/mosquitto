9a97a5afc414b2950934d0fc1ab2dcebca0ce27f
Roger A. Light <roger@atchoo.org>
2021-08-04 17:30:24 +0100

    Improve client protocol version number logging.
    Protocol version numbers reported in the log when a client connects now
    match the MQTT protocol version numbers, not internal Mosquitto values.

--- lib/mosquitto_internal.h
+++ lib/mosquitto_internal.h
@@ -119,9 +119,9 @@ enum mosquitto_client_state {
 
 enum mosquitto__protocol {
 	mosq_p_invalid = 0,
-	mosq_p_mqtt31 = 1,
-	mosq_p_mqtt311 = 2,
-	mosq_p_mqtts = 3,
+	mosq_p_mqtts = 1,
+	mosq_p_mqtt31 = 3,
+	mosq_p_mqtt311 = 4,
 	mosq_p_mqtt5 = 5,
 };
 
