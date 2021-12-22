0fc3f6c0f046ec84c2ed9aca750063cb188c7cdf
Scott Zhao <zhaomh1998@outlook.com>
2021-12-22 04:39:10 -0500

    disconnect with reason code proof

---
+++ test/cbmc/proof_disconnect/cbmc.sh
@@ -0,0 +1,12 @@
+make clean
+make proof.gb
+goto-instrument proof.gb proof_new.gb \
+    --generate-function-body '(?!__)str[a-zA-Z]*' --generate-function-body-options nondet-return \
+    # --remove-function-body 'packet__write'
+
+cbmc proof_new.gb --function harness --pointer-check \
+    --pointer-check --pointer-primitive-check --pointer-overflow-check \
+    --signed-overflow-check --unsigned-overflow-check --float-overflow-check \
+    --undefined-shift-check --nan-check \
+    --stop-on-fail --trace
+    # --memory-leak-check --malloc-may-fail --malloc-fail-null --unwind 10
---
+++ test/cbmc/proof_disconnect/proof.c
@@ -0,0 +1,40 @@
+#include "mosquitto.h"
+#include "mosquitto_internal.h"
+#include "mosquitto_broker_internal.h"
+#include "memory_mosq.h"
+#include "mqtt_protocol.h"
+#include <assert.h>
+#include <limits.h>
+
+char byte_1_reference = 0x00;
+char byte_2_reference = 0x00;
+char byte_3_reference = 0x00;
+
+void verify_buffer(const void *buf) {
+	char b1 = ((char *)buf)[0];
+	char b2 = ((char *)buf)[1];
+	char b3 = ((char *)buf)[2];
+	assert(b1 == byte_1_reference);
+	assert(b2 == byte_2_reference);
+	assert(b3 == byte_3_reference);
+}
+
+void harness()
+{
+
+    struct mosquitto *mosq;
+
+    // Set up mosquitto context
+    mosq = (struct mosquitto *)mosquitto__calloc(1, sizeof(struct mosquitto));
+    // mosq->state = mosq_cs_active;
+
+    mosq->protocol = mosq_p_mqtt5;
+    mosq->in_packet.command = CMD_PINGREQ;
+
+    byte_1_reference = 0xE0; // disconnect (11100000)
+    byte_2_reference = 0x01; // remaining length (00000001)
+    byte_3_reference = 0x82; // Protocol error (10000010) with wrong mosq->state
+    int ret = handle__packet(mosq);;
+    assert(ret == MOSQ_ERR_PROTOCOL);
+
+}
